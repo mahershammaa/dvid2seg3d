e5ec91ed35a3120919abfa64cf79a425a187c925
Maher Shammaa <amshammaa@mix.wvu.edu>
2016-04-27 17:41:49 -0400

    updated install

--- install.sh
+++ install.sh
@@ -7,6 +7,25 @@ cd lz4
 make
 sudo make install
 
+cd ..
+
 sudo apt-get install libjsoncpp-dev 
 
 sudo apt-get install libboost-all-dev
+
+git clone https://github.com/janelia-flyem/libdvid-cpp.git
+
+cd libdvid-cpp
+
+mkdir build 
+
+cd build
+
+cmake -DLIBDVID_WRAP_PYTHON=1 ..
+
+make 
+
+sudo make install
+
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
+
